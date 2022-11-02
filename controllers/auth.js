82d43381ea93cf5fcc912784666737eec78a8e68
AnkitGaikwad <ankit.gaikwad@wohlig.com>
2022-11-02 17:13:29 +0530

    validation using ajv

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const {BadRequestError,UnauthenticatedError} = require('../errors');
 var jsonValidator = require('jsonschema').Validator;
+const Ajv = require("ajv");
 
 const register = async (req, res) => {
     const user = await User.create({...req.body});
@@ -11,31 +12,45 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     var userSchema = {  
-        id: '/User',  
         type: 'object',  
         properties: {    
             username: { type: 'string' },
             email: {
-                type: 'string',      
-                format: 'email'    
+                type: 'string',     
             },    
             password: { type: 'string' }  
         },  
-        required: ['email', 'password']
+        required: ['email', 'password'],
+        additionalProperties: false
     };
 
-    var validator = new jsonValidator();
-    validator.addSchema(userSchema, '/User');
-    try {    
-        validator.validate(req.body, userSchema, { throwError: true });  
-    } catch (error) {    
-        res.status(StatusCodes.UNAUTHORIZED).end('Invalid body format: ' + error.message);    
-        return;  
+    // Validation of req.body using jsonSchema module
+    // var validator = new jsonValidator();
+    // validator.addSchema(userSchema, '/User');
+    // try {    
+    //     validator.validate(req.body, userSchema, { throwError: true });  
+    // } catch (error) {    
+    //     res.status(StatusCodes.UNAUTHORIZED).end('Invalid body format: ' + error.message);    
+    //     return;  
+    // }
+
+    //Validation of req.body using Ajv module
+    const ajv = new Ajv({allErrors: true});
+    try { 
+        const validate = ajv.addSchema(userSchema).compile(userSchema);
+        const valid = validate(req.body);
+        if (!valid) {
+            throw validate.errors[0].message;
+        }
+    } catch (error) {
+        res.status(StatusCodes.UNAUTHORIZED).end('Invalid body format: ' + error);    
+        return; 
     }
-
-    const {email, password} = req.body;
+    
+    const {email, password} = req.body; 
+    console.log(password);
     const user = await User.findOne({email});
-
+    console.log(user);
     if(!user) {
         throw new UnauthenticatedError('Invalid credentials');
     }
